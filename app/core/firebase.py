ce521eb98327d72d362541c30044ea365cf0d495
Nguyen Van Quy <nquy50771@gamil.com>
2026-02-19 18:58:17 +0700

    redeploy

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -3,16 +3,13 @@ from firebase_admin import credentials
 import os
 import json
 
-firebase_json_str = os.getenv("FIREBASE_CONFIG_JSON")
-
-if firebase_json_str:
-    firebase_info = json.loads(firebase_json_str)
-    cred = credentials.Certificate(firebase_info)
-    firebase_admin.initialize_app(cred)
-else:
-    print("LỖI: Biến môi trường FIREBASE_CONFIG_JSON chưa được thiết lập!")
-
 def init_firebase():
-    if not firebase_admin._apps:
-        cred = credentials.Certificate(firebase_json_str)
-        firebase_admin.initialize_app(cred)
+    firebase_json_str = os.getenv("FIREBASE_CONFIG_JSON")
+
+    if not firebase_json_str:
+        raise Exception("FIREBASE_CONFIG_JSON chưa được thiết lập")
+
+    firebase_dict = json.loads(firebase_json_str)
+
+    cred = credentials.Certificate(firebase_dict)
+    firebase_admin.initialize_app(cred)
